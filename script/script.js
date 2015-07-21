108de3a4419589e049f92adeb602a5c0de34ceb7
Pavlo <kasianchuk.pavlo@gmail.com>
2015-07-21 16:47:36 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,91 +1,35 @@
 $(function(){
-	var locale = 'ua',
-		weatherDiv = $('#weather'),
-		scroller = $('#scroller'),
-		location = $('.location');
+    getWeatherData('ua', dataReceived, showError);
 
-	getWeatherData(locale, dataReceived, showError);
+    function dataReceived(data) {
+        var offset = (new Date()).getTimezoneOffset()*60*1000; 
+        var city = data.city.name;
+        var country = data.city.country;
+    
+        var localTime = new Date(data.list[0].dt*1000 - offset); 
+            addWeather(
+                data.list[0].weather[0].icon,
+                moment(localTime).calendar(),	
+                data.list[0].weather[0].description,
+                Math.round(data.list[0].temp.day) + '&deg;C',
+                data.list[0].pressure,
+                data.list[0].humidity,
+                data.list[0].speed
+        );
+        $('.location').html(city + ', <b>' + country + '</b>'); 
+    }
 
-	function dataReceived(data) {
-		// Get the offset from UTC (turn the offset minutes into ms)
-		var offset = (new Date()).getTimezoneOffset()*60*1000;
-		var city = data.city.name;
-		var country = data.city.country;
+    function addWeather(icon, day, condition, temp, pressure, humidity, wind){            
+        $('#day').html(day);
+        $('#icon').html('<img width="90" height="90" src="icons/'+ icon +'.png" />');
+        $('#temperature').html(temp);
+        $('#condition').html(condition);    
+        $('#pressure').html(pressure);
+        $('#humidity').html(humidity);
+        $('#wind').html(wind);
+    };
 
-		$.each(data.list, function(){
-			// "this" holds a forecast object
-			// Get the local time of this forecast (the api returns it in utc)
-			var localTime = new Date(this.dt*1000 - offset);
-			addWeather(
-				this.weather[0].icon,
-				moment(localTime).calendar(),	// We are using the moment.js library to format the date
-				this.weather[0].description + ' <b>' + Math.round(this.temp.day) + '°C' + '</b>'
-			);
-		});
-		// Add the location to the page
-		location.html(city+', <b>'+country+'</b>');
-		weatherDiv.addClass('loaded');
-		// Set the slider to the first slide
-		showSlide(0);
-	}
-
-	function addWeather(icon, day, condition){
-
-		var markup = '<li>'+
-			'<img src="assets/img/icons/'+ icon +'.png" />'+
-			' <p class="day">'+ day +'</p> <p class="cond">'+ condition +
-			'</p></li>';
-
-		scroller.append(markup);
-	}
-
-	/* Handling the previous / next arrows */
-	var currentSlide = 0;
-	weatherDiv.find('a.previous').click(function(e){
-		e.preventDefault();
-		showSlide(currentSlide-1);
-	});
-
-	weatherDiv.find('a.next').click(function(e){
-		e.preventDefault();
-		showSlide(currentSlide+1);
-	});
-
-	// listen for arrow keys
-	$(document).keydown(function(e){
-		switch(e.keyCode){
-			case 37: 
-				weatherDiv.find('a.previous').click();
-			break;
-			case 39:
-				weatherDiv.find('a.next').click();
-			break;
-		}
-	});
-
-	function showSlide(i){
-		var items = scroller.find('li');
-
-		if (i >= items.length || i < 0 || scroller.is(':animated')){
-			return false;
-		}
-
-		weatherDiv.removeClass('first last');
-
-		if(i == 0){
-			weatherDiv.addClass('first');
-		}
-		else if (i == items.length-1){
-			weatherDiv.addClass('last');
-		}
-
-		scroller.animate({left:(-i*100)+'%'}, function(){
-			currentSlide = i;
-		});
-	}
-
-	/* Error handling functions */
-	function showError(msg){
-		weatherDiv.addClass('error').html(msg);
-	}
+    function showError(msg){
+        $('#error').html('Виникла помилка: ' + msg);
+    }
 });
